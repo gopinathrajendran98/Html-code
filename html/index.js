65455bfd760c4939e562389124c493e9a50b5ff2
gopinathrajendran98 <89033905+gopinathrajendran98@users.noreply.github.com>
2024-01-19 15:06:43 +0530

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,9 @@
+// const person={name:'gopi',email:'gopinathrajendran@'};
+// const allPerson={...person};
+// console.log(allPerson)
+// person===allPerson;
+
+// promise.resolve(5)
+// .then(res => res *2)
+// .then(res => res *2)
+// console.log(res); 
